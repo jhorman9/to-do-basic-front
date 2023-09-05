ab30046373b7729a42b522bf8c36df815cff6b0d
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2023-09-04 23:57:36 -0500

    adding server

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -7,7 +7,6 @@ const Tasks = ({ data, getAllToDo }) => {
     
     axios.put(`https://to-do-crud-3kmn-dev.fl0.io/todo/${data.id}`, { isCompleted: e.target.checked })
         .then(res => {
-            console.log(res.data);
             getAllToDo();
         })
         .catch(error => console.log(error));
@@ -26,7 +25,7 @@ const Tasks = ({ data, getAllToDo }) => {
     <>
     <div className="card-header">
         { data.isCompleted == true  ? <span className='fw-bold' style={{color: "green"}}>Completado</span> : <span className='fw-bold' style={{color: "#5f0000"}}>No completado</span>}
-        <span onClick={deleteData} className='delete fw-bold'>x</span>
+        <span onClick={deleteData} className='delete fw-bold' style={{cursor: 'pointer'}}>x</span>
     </div>
     <div className="card-body" style={data.isCompleted ? {background: 'green'} : {background: '#5f0000', color: 'white'} }>
         <h5 className="card-title">{data.title}</h5>
